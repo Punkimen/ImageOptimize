d6800877848a202570d3f7acfea0e75bcd902278
Max <punkimen666@gmail.com>
2024-07-24 17:51:38 +0300

    optimize

---
+++ index.js
@@ -0,0 +1,42 @@
+const sharp = require('sharp');
+const fs = require('fs');
+
+const inputFolder = 'images';
+const outputFolder = 'optimizeImage';
+
+const formats = ['webp'];
+
+if (!fs.existsSync(inputFolder)) {
+    fs.mkdirSync(outputFolder)
+}
+
+
+fs.readdir(inputFolder, (err, files) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    files.forEach(file => {
+        if (file.endsWith('.jpg') || file.endsWith('.jpeg') || file.endsWith('.png')) {bg_main
+            const inputPath = `${inputFolder}/${file}`;
+            const name = file.substring(0, file.lastIndexOf('.'));
+
+            formats.forEach(format => {
+                const outputPath = `${outputFolder}/${name}.${format}`;
+
+                if (!fs.existsSync(outputPath)) {
+                    sharp(inputPath)
+                        .toFormat(format, {quality: 95})
+                        .toFile(outputPath, (err) => {
+                            if (err) {
+                                console.error(err);
+                            } else {
+                                console.log(`${name}.${format} saved`);
+                            }
+                        });
+                }
+            });
+        }
+    });
+});
