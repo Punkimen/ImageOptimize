909d1247cd919990a62adc399afb97e56f7a63a1
Max <punkimen666@gmail.com>
2024-07-24 17:57:22 +0300

    fix

--- index.js
+++ index.js
@@ -7,36 +7,36 @@ const outputFolder = 'optimizeImage';
 const formats = ['webp'];
 
 if (!fs.existsSync(inputFolder)) {
-    fs.mkdirSync(outputFolder)
+  fs.mkdirSync(outputFolder)
 }
 
 
 fs.readdir(inputFolder, (err, files) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-    files.forEach(file => {
-        if (file.endsWith('.jpg') || file.endsWith('.jpeg') || file.endsWith('.png')) {bg_main
-            const inputPath = `${inputFolder}/${file}`;
-            const name = file.substring(0, file.lastIndexOf('.'));
+  files.forEach(file => {
+    if (file.endsWith('.jpg') || file.endsWith('.jpeg') || file.endsWith('.png')) {
+      const inputPath = `${inputFolder}/${file}`;
+      const name = file.substring(0, file.lastIndexOf('.'));
 
-            formats.forEach(format => {
-                const outputPath = `${outputFolder}/${name}.${format}`;
+      formats.forEach(format => {
+        const outputPath = `${outputFolder}/${name}.${format}`;
 
-                if (!fs.existsSync(outputPath)) {
-                    sharp(inputPath)
-                        .toFormat(format, {quality: 95})
-                        .toFile(outputPath, (err) => {
-                            if (err) {
-                                console.error(err);
-                            } else {
-                                console.log(`${name}.${format} saved`);
-                            }
-                        });
-                }
+        if (!fs.existsSync(outputPath)) {
+          sharp(inputPath)
+            .toFormat(format, {quality: 95})
+            .toFile(outputPath, (err) => {
+              if (err) {
+                console.error(err);
+              } else {
+                console.log(`${name}.${format} saved`);
+              }
             });
         }
-    });
+      });
+    }
+  });
 });
